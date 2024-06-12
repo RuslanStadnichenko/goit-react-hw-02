f9ca47fd7473050ecf9f6c1fa1e38abdfda886e0
RuslanStadnichenko <imanpepsi77@gmail.com>
2024-06-12 10:27:45 +0300

    finish

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,13 +3,13 @@ import s from "./Options.module.css";
 export const Options = ({updateFeedback, totalFeedback, resetFeedback}) => {
     return (
     <ul className={s.ulbuttons}>
-        <li><button className={s.button}   onClick={() => {
+        <li><button className={s.buttonG}   onClick={() => {
           updateFeedback("good");
         }}>Good</button></li>
-        <li><button className={s.button}   onClick={() => {
+        <li><button className={s.buttonN}   onClick={() => {
           updateFeedback("neutral");
         }}>Neutral</button></li>
-        <li><button className={s.button}   onClick={() => {
+        <li><button className={s.buttonB}   onClick={() => {
           updateFeedback("bad");
         }}>Bad</button></li>
             <li>
